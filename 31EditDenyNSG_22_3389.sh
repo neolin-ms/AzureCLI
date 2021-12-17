340ee531236fbde668becd7040ee85b7f0cec945
Neo Lin <neo.lin@microsoft.com>
2021-12-17 19:18:57 +0800

    update No.31 script

--- 31EditDenyNSG_22_3389.sh
+++ 31EditDenyNSG_22_3389.sh
@@ -22,23 +22,18 @@
 rg_word_count=$(az group list --query [].name -o tsv | wc -l)
 for ((i = 1 ; i <= ${rg_word_count} ; i++)); do
   rg_name=$(az group list --query [].name -o tsv | sed -n ${i}p)
-  echo "Resource Group ${i}: ${rg_name}"
+  echo "Resource Group Name ${i}: ${rg_name}"
   nsg_name=$(az network nsg list --resource-group ${rg_name} --query [].name -o tsv)
   if [[ -z "${nsg_name}" ]]; then
     echo "This Resource Group hasn't any NSGs."
+    echo "------------------------------------"
   elif [[ -n "${nsg_name}" ]]; then
-    if [[ testWindowsRg == ${rg_name} ]]; then
-      echo "This is a Windows resource group"
-      echo "NSG: ${nsg_name}"
-    else
-      echo "This is a Linux resource group" 
-      nsg_word_count=$(az network nsg list --resource-group ${rg_name} --query [].name -o tsv | wc -l)
-      for ((j = 1 ; j <= ${nsg_word_count} ; j++)); do
+    nsg_word_count=$(az network nsg list --resource-group ${rg_name} --query [].name -o tsv | wc -l)
+    for ((j = 1 ; j <= ${nsg_word_count} ; j++)); do
          nsg_name=$(az network nsg list --resource-group ${rg_name} --query [].name -o tsv | sed -n ${j}p)
-         echo "NSG: ${nsg_name}"
+         echo "NSG Name: ${nsg_name}"
          dest_port=$(az network nsg rule list -g ${rg_name} --nsg-name ${nsg_name} --query [].destinationPortRange -o tsv)
          echo "Destination Port Range: ${dest_port}"
-      done
-    fi
+    done
   fi
 done
