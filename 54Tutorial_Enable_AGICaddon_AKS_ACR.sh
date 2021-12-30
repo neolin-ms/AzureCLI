999f3d879fffa397b61c3044b296254731902bfd
Neo Lin <neo.lin@microsoft.com>
2021-12-30 23:06:01 +0800

    update No.54 script

--- 54Tutorial_Enable_AGICaddon_AKS_ACR.sh
+++ 54Tutorial_Enable_AGICaddon_AKS_ACR.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
-==Step.1 Prepare application for AKS (https://docs.microsoft.com/en-us/azure/aks/tutorial-kubernetes-prepare-app)
+# References
+## Step.1 Prepare application for AKS, https://docs.microsoft.com/en-us/azure/aks/tutorial-kubernetes-prepare-app
+## Step.2 Create continer registry, https://docs.microsoft.com/en-us/azure/aks/tutorial-kubernetes-prepare-acr?tabs=azure-cli
+## Step.3 Create Kubernetes cluster, https://docs.microsoft.com/en-us/azure/aks/tutorial-kubernetes-deploy-cluster?tabs=azure-cli
+## Step.4 Run application, https://docs.microsoft.com/en-us/azure/aks/tutorial-kubernetes-deploy-application?tabs=azure-cli 
+## Step.5 Ingress Controller Add-on for AKS (Brownfield), https://docs.microsoft.com/en-us/azure/application-gateway/tutorial-ingress-controller-add-on-existing
+## Step.5 Generate TLS certificates, https://docs.microsoft.com/en-us/azure/aks/ingress-own-tls#generate-tls-certificates
+## Step.5 Expose an AKS service over HTTP or HTTPS using Application Gateway, https://docs.microsoft.com/en-us/azure/application-gateway/ingress-controller-expose-service-over-http-https
+
+# Step.1 Prepare application for AKS
 
 git clone https://github.com/Azure-Samples/azure-voting-app-redis.git
 
@@ -14,7 +23,7 @@ docker ps
 
 docker-compose down
 
-==Step.2 Create continer registry (https://docs.microsoft.com/en-us/azure/aks/tutorial-kubernetes-prepare-acr?tabs=azure-cli)
+# Step.2 Create continer registry
 
 az group create --name myResourceGroup --location eastus
 
@@ -34,7 +43,7 @@ az acr repository list --name neotestacr20211230 --output table
 
 az acr repository show-tags --name neotestacr20211230 --repository azure-vote-front --output table
 
-==Step.3 Create Kubernetes cluster (https://docs.microsoft.com/en-us/azure/aks/tutorial-kubernetes-deploy-cluster?tabs=azure-cli)
+# Step.3 Create Kubernetes cluster
 
 az aks create \
     --resource-group myResourceGroup \
@@ -49,7 +58,7 @@ az aks get-credentials --resource-group myResourceGroup --name myAKSCluster
 
 kubectl get nodes
 
-==Step.4 Run application (for test)
+# Step.4 Run application (for test)
 
 az acr list --resource-group myResourceGroup --query "[].{acrLoginServer:loginServer}" --output table
 
@@ -68,20 +77,20 @@ kubectl get service azure-vote-front --watch
 
 kubectl delete -f azure-vote-all-in-one-redis.yaml
 
-==Step.5 Ingress Controller Add-on for AKS (Brownfield)(https://docs.microsoft.com/en-us/azure/application-gateway/tutorial-ingress-controller-add-on-existing)
+# Step.5 Ingress Controller Add-on for AKS (Brownfield)
 
-Step.5.1 Deploy a new Application Gateway
+## Step.5.1 Deploy a new Application Gateway
 
 az network public-ip create -n myPublicIp -g myResourceGroup --allocation-method Static --sku Standard
 az network vnet create -n myVnet -g myResourceGroup --address-prefix 11.0.0.0/8 --subnet-name mySubnet --subnet-prefix 11.1.0.0/16 
 az network application-gateway create -n myApplicationGateway -l eastus -g myResourceGroup --sku Standard_v2 --public-ip-address myPublicIp --vnet-name myVnet --subnet mySubnet
 
-Step.5.2 Enable the AGIC add-on in existing AKS cluster through Azure CLI
+## Step.5.2 Enable the AGIC add-on in existing AKS cluster through Azure CLI
 
 appgwId=$(az network application-gateway show -n myApplicationGateway -g myResourceGroup -o tsv --query "id") 
 az aks enable-addons -n myAKSCluster -g myResourceGroup -a ingress-appgw --appgw-id $appgwId
 
-Step.5.3 Peer the two virtual networks together
+## Step.5.3 Peer the two virtual networks together
 
 nodeResourceGroup=$(az aks show -n myAKSCluster -g myResourceGroup -o tsv --query "nodeResourceGroup")
 aksVnetName=$(az network vnet list -g $nodeResourceGroup -o tsv --query "[0].name")
@@ -92,10 +101,12 @@ az network vnet peering create -n AppGWtoAKSVnetPeering -g myResourceGroup --vne
 appGWVnetId=$(az network vnet show -n myVnet -g myResourceGroup -o tsv --query "id")
 az network vnet peering create -n AKStoAppGWVnetPeering -g $nodeResourceGroup --vnet-name $aksVnetName --remote-vnet $appGWVnetId --allow-vnet-access
 
-Step.5.4 Deploy a sample application using AGIC
+## Step.5.4 Deploy a sample application using AGIC
 
 kubectl apply -f https://raw.githubusercontent.com/Azure/application-gateway-kubernetes-ingress/master/docs/examples/aspnetapp.yaml
 
 kubectl get ingress
 
 kubectl delete -f https://raw.githubusercontent.com/Azure/application-gateway-kubernetes-ingress/master/docs/examples/aspnetapp.yaml
+
+## Step.5.5 Deploy a sample application using AGIC with HTTPS
