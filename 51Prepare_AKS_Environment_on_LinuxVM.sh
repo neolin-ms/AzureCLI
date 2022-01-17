81bcea503c57bd34146f98f07be4cc622ba0ec2a
Neo Lin <neo.lin@microsoft.com>
2022-01-17 10:52:33 +0800

    rename script name and add new script for prepare the AKS environment

---
+++ 51Prepare_AKS_Environment_on_LinuxVM.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+# Install the AzureCLI on Linux - Install with one command (https://docs.microsoft.com/en-us/cli/azure/install-azure-cli)
+curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
+
+# Login to Azure
+az login --use-device-code
+
+# Confirm AKS cluster is using managed identity (https://docs.microsoft.com/en-us/azure/aks/use-managed-identity)
+az aks show -g myResourceGroup -n myCluster --query "servicePrincipalProfile"
+
+az aks show -g StagingGroup -n planStagingCluster --query "servicePrincipalProfile"
+
+# Find the control plane system-assigned identity's object ID
+az aks show -g StagingGroup -n planStagingCluster --query "identity"
+
+# Docker install on ubuntu (https://docs.docker.com/engine/install/ubuntu/)
+sudo apt-get update
+
+sudo apt-get install \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release
+	
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg	
+
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+  
+sudo apt-get install docker-ce docker-ce-cli containerd.io  
+
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io
+
+sudo docker run hello-world
+
+# Install Docker Compose on Linux systems (https://docs.docker.com/compose/install/)
+sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+
+sudo chmod +x /usr/local/bin/docker-compose
+
+sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+
+docker-compose --version
+
+# Configure ACR integration for existing AKS clusters
+
+az aks update -n myCluster -g myResourceGroup --attach-acr acrtestneo20211230
