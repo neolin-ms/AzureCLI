ec9e0c7ece53a99be80c3a0526ecc2a61a149df6
Neo Lin <neo.lin@microsoft.com>
2022-01-18 11:40:06 +0800

    update No.56 script

--- 56Use_with_Your_Own_Certificates.sh
+++ 56Use_with_Your_Own_Certificates.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
 
 # Create a new AKS cluster with ACR integration 
-# https://docs.microsoft.com/en-us/azure/aks/cluster-container-registry-integration?tabs=azure-cli#create-a-new-aks-cluster-with-acr-integration
+## https://docs.microsoft.com/en-us/azure/aks/cluster-container-registry-integration?tabs=azure-cli#create-a-new-aks-cluster-with-acr-integration
 
-## set this to the name of your Azure Container Registry.  It must be globally unique
+# Step.1 Create the name of your Azure Container Registry,and integration with AKS cluster. 
+
+## It must be globally unique.
 MYACR=testmyacr0117
 
 ## Create a resource group
 az group create --name myResourceGroup --location eastus
 
-## Run the following line to create an Azure Container Registry if you do not already have one
+## Run the following line to create an Azure Container Registry if you do not already have one.
 az acr create -n $MYACR -g myResourceGroup --sku basic
 
-## Create an AKS cluster with ACR integration
+## Create an AKS cluster with ACR integration.
 az aks create -n myAKSCluster \
   --resource-group myResourceGroup \
   --node-count 2 \
@@ -22,7 +24,7 @@ az aks create -n myAKSCluster \
 az aks get-credentials --resource-group myResourceGroup --name myAKSCluster
 kubectl get nodes -o wide
 
-# Use TLS with your own certificates (https://docs.microsoft.com/en-us/azure/aks/ingress-own-tls?tabs=azure-cli)
+# Step.2 Use TLS with your own certificates (https://docs.microsoft.com/en-us/azure/aks/ingress-own-tls?tabs=azure-cli)
 
 ## Import the images used by the Helm chart into your ACR
 REGISTRY_NAME=testmyacr0117
@@ -38,14 +40,14 @@ az acr import --name $REGISTRY_NAME --source $SOURCE_REGISTRY/$CONTROLLER_IMAGE:
 az acr import --name $REGISTRY_NAME --source $SOURCE_REGISTRY/$PATCH_IMAGE:$PATCH_TAG --image $PATCH_IMAGE:$PATCH_TAG
 az acr import --name $REGISTRY_NAME --source $SOURCE_REGISTRY/$DEFAULTBACKEND_IMAGE:$DEFAULTBACKEND_TAG --image $DEFAULTBACKEND_IMAGE:$DEFAULTBACKEND_TAG
 
-# List container images (https://docs.microsoft.com/en-us/azure/container-registry/container-registry-get-started-azure-cli#list-container-images)
+## List container images (https://docs.microsoft.com/en-us/azure/container-registry/container-registry-get-started-azure-cli#list-container-images)
 ## Get the login server address
 az acr list --resource-group myResourceGroup --query "[].{acrLoginServer:loginServer}" --output table
 
 ## List images in registry
 az acr repository list --name $MYACR --output table
 
-# Create an ingress controller
+# Step.3 Create an ingress controller
 
 ## Add the ingress-nginx repository
 helm repo add ingress-nginx https://kubernetes.github.io/ingress-nginx
@@ -74,7 +76,7 @@ helm install nginx-ingress ingress-nginx/ingress-nginx \
     --set defaultBackend.image.tag=$DEFAULTBACKEND_TAG \
     --set defaultBackend.image.digest=""
 
-# Create Kubernetes secret for the TLS certificate
+# Step.4 Create Kubernetes secret for the TLS certificate
 kubectl create secret tls aks-ingress-tls \
     --namespace dotapi \
     --key aks-ingress-tls.key \
@@ -82,18 +84,18 @@ kubectl create secret tls aks-ingress-tls \
 	
 kubectl get secret -n dotapi
 
-###==Run only one demo application(https://docs.microsoft.com/en-us/azure/aks/ingress-own-tls?tabs=azure-cli#run-demo-applications)==##
+# Step.5 Run only one demo application and create ingress route(https://docs.microsoft.com/en-us/azure/aks/ingress-own-tls?tabs=azure-cli#run-demo-applications)
 
-#Run demo application 
+## Run demo application 
 kubectl apply -f aks-helloworld.yaml --namespace dotapi
 
-# Create an ingress route for one demo application
+## Create an ingress route for one demo application
 kubectl apply -f hello-world-ingress-1.yaml --namespace dotapi
 
-# Test the ingress configuration
+## Test the ingress configuration
 curl -v -k --resolve demo.azure.com:443:52.224.145.250 https://demo.azure.com
 
-###==Run two demo applications(https://docs.microsoft.com/en-us/azure/aks/ingress-own-tls?tabs=azure-cli#run-demo-applications)==##
+# Step.6 (optional)Run two demo applications and create ingress route(https://docs.microsoft.com/en-us/azure/aks/ingress-own-tls?tabs=azure-cli#run-demo-applications)
 
 # Run demo applications
 kubectl apply -f aks-helloworld.yaml --namespace dotapi
